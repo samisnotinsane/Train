042491d247338269ef913f6a6529f0501ccfe603
samisnotinsane <sameen11@hotmail.com>
2018-09-15 20:44:29 +0100

    Adding method for reading train feed file.

---
+++ src/main/java/info/sameen/TrainFeed.java
@@ -0,0 +1,53 @@
+package info.sameen;
+
+/**
+ * Created by sameen on 15/09/2018.
+ */
+public class TrainFeed {
+    private String trainType;
+    private int gear;
+    private int speed;
+    private int energy;
+
+    public TrainFeed() {
+    }
+
+    public TrainFeed(String trainType, int gear, int speed, int energy) {
+        this.trainType = trainType;
+        this.gear = gear;
+        this.speed = speed;
+        this.energy = energy;
+    }
+
+    public String getTrainType() {
+        return trainType;
+    }
+
+    public void setTrainType(String trainType) {
+        this.trainType = trainType;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+}
---
+++ src/test/java/TrainFeedTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by sameen on 15/09/2018.
+ */
+public class TrainFeedTest {
+
+    @Test
+    public void testTrainType() {
+
+    }
+
+    @Test
+    public void testGear() {
+
+    }
+
+    @Test
+    public void testSpeed() {
+
+    }
+
+    @Test
+    public void testTrainEnergy() {
+
+    }
+
+}
