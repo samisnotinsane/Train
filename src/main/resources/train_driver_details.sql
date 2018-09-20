e6149b572221e29d0c3df3b8bc269fcfeaca9575
samisnotinsane <sameen11@hotmail.com>
2018-09-20 23:20:15 +0100

    Adding sql script for train driver details.

---
+++ src/main/resources/train_driver_details.sql
@@ -0,0 +1,2 @@
+CREATE TABLE train_driver_details (train_id VARCHAR (10) PRIMARY KEY NOT NULL,
+   from_station VARCHAR (10), to_station VARCHAR (10), driver_name VARCHAR (10), journey_status VARCHAR (10));
