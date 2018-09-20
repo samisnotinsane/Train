3f82a8f8e9ba7e1c2f9594a49bd0faf40f0210c9
samisnotinsane <sameen11@hotmail.com>
2018-09-20 23:31:00 +0100

    Adding script for train delay details.

---
+++ src/main/resources/train_delay_details.sql
@@ -0,0 +1,2 @@
+CREATE TABLE train_delay_details (train_id VARCHAR (10) PRIMARY KEY NOT NULL,
+  station VARCHAR (10), departure_time_at_station VARCHAR (10), departure_lateness_in_seconds VARCHAR (10));
