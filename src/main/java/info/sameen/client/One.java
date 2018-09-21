8084e007bb13d45580cad1ef3aff44c3f3e7a8b1
samisnotinsane <sameen11@hotmail.com>
2018-09-21 11:42:44 +0100

    Adding client to get max speed and lowest energy.

--- src/main/java/info/sameen/client/One.java
+++ src/main/java/info/sameen/client/One.java
@@ -1,7 +1,37 @@
 package info.sameen.client;
 
-/**
- * Created by sameen on 21/09/2018.
- */
+import info.sameen.model.TrainFeed;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class One {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        try {
+            TrainFeed trainFeed = new TrainFeed();
+
+            System.out.println("Train type, Gear: ");
+            String[] trainTypeGear = input.nextLine()
+                    .trim()
+                    .split(",");
+
+            String maxSpeedStr = trainFeed.maxSpeedStr(trainTypeGear[0],
+                    Integer.parseInt(trainTypeGear[1].trim()));
+            System.out.println("Maximum speed: " + maxSpeedStr);
+            System.out.println("------");
+
+            System.out.println("Gear: ");
+            int gear = Integer.parseInt(input.nextLine());
+            String minEnergyStr = trainFeed.minEnergyStr(gear);
+            System.out.println("Train with lowest energy consumption: " + minEnergyStr);
+            System.out.println("------");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
